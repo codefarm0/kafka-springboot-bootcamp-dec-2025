63ae4ba1bed5db8deab5d32f5cbb97ab19a6313f
Codefarm <whytechnology01@gmail.com>
2025-12-24 20:42:40 +0530

    rough

---
+++ src/main/java/in/codefarm/order/service/as/producer/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package in.codefarm.order.service.as.producer.repository;
+
+import in.codefarm.order.service.as.producer.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    Optional<OrderEntity> findByOrderId(String orderId);
+}
+
