f8ed0d24589828f2ee91218e060abae1ed8e49ef
Codefarm <whytechnology01@gmail.com>
2025-12-24 20:26:58 +0530

    initial commit

---
+++ exploration/notificaion-service-as-consumer/src/main/java/in/codefarm/notificaion/service/as/consumer/NotificaionServiceAsConsumerApplication.java
@@ -0,0 +1,13 @@
+package in.codefarm.notificaion.service.as.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificaionServiceAsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificaionServiceAsConsumerApplication.class, args);
+	}
+
+}
